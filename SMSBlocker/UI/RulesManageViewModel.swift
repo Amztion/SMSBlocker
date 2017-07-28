d583349c9f72da8a3b17475419b20d4d3f9aac9e
Amztion <amztion@gmail.com>
2017-07-29 00:15:35 +0800

    [~]Modify UI Storyboard, add ViewController, ViewModel

---
+++ SMSBlocker/UI/RulesManageViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RulesManageViewModel.swift
+//  SMSBlocker
+//
+//  Created by Liang Zhao on 2017/7/28.
+//  Copyright © 2017年 Liang Zhao. All rights reserved.
+//
+
+import UIKit
+
+class RulesManageViewModel: NSObject {
+
+}
