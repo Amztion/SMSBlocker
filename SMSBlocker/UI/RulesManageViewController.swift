f03d9f197878ca088701f096109ea9c15a1d17e1
Amztion <amztion@gmail.com>
2017-07-31 23:48:38 +0800

    [~]Modify UI

--- SMSBlocker/UI/RulesManageViewController.swift
+++ SMSBlocker/UI/RulesManageViewController.swift
@@ -16,12 +16,6 @@ class RulesManageViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
     /*
     // MARK: - Navigation
 
@@ -31,5 +25,22 @@ class RulesManageViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+}
 
+extension RulesManageViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+}
+
+extension RulesManageViewController: UITableViewDelegate {
+    
 }
